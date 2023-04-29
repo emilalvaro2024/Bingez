5111b00c4757f9fdf98900138f044dce72f1e51d
Koushik Ghosh <55987395+koushikghosh11@users.noreply.github.com>
2023-04-29 11:36:54 +0530

    added error.handlebars and restructured the program with express-generator generated program

--- server.js
+++ server.js
@@ -1,47 +1,35 @@
-// initiating router files
 const express = require('express'),
-    app = express(),
     expHbs = require('express-handlebars'),
     path = require('path'),
-    port = normalizePort(process.env.PORT || '3000'),
-    home = require('./routes/home'),
-    movie = require('./routes/movie'),
-    series = require('./routes/series'),
-    actor = require('./routes/actor'),
-    search = require('./routes/search');
+
+    cookieParser = require('cookie-parser'),
+    logger = require('morgan'),
+
+    // initiating router files
+    homeRouter = require('./routes/home'),
+    movieRouter = require('./routes/movie'),
+    seriesRouter = require('./routes/series'),
+    actorRouter = require('./routes/actor'),
+    searchRouter = require('./routes/search'),
+
+    app = express();
 
 // view engine is handlebars
 app.engine('handlebars', expHbs.engine())
     .set('view engine', 'handlebars')
 
 // serve static files
-    .use('/static',express.static(path.join(__dirname, 'static')))
+    .use(logger('dev'))
+    .use(express.json())
+    .use(express.urlencoded({ extended: false }))
+    .use(cookieParser())
+    .use('/static', express.static(path.join(__dirname, 'static')))
 
 // using router middleware
-    .use('/', home)
-    .use('/movie', movie)
-    .use('/series', series)
-    .use('/actor', actor)
-    .use('/search', search)
+    .use('/', homeRouter)
+    .use('/movie', movieRouter)
+    .use('/series', seriesRouter)
+    .use('/actor', actorRouter)
+    .use('/search', searchRouter);
 
-// app listen on port
-    .listen(port, (err) => {
-    if (err) throw err;
-    console.log(`listening on port ${port}`)
-})
-
-function normalizePort(val) {
-    let port = parseInt(val, 10);
-
-    if (isNaN(port)) {
-        // named pipe
-        return val;
-    }
-
-    if (port >= 0) {
-        // port number
-        return port;
-    }
-
-    return false;
-}
+module.exports = app;
